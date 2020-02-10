064b0a5b6aa035481bc25ea1bcc511a7955eefd7
Loki2236 <28462447+loki2236@users.noreply.github.com>
2020-02-10 16:46:22 -0300

    First Version

---
+++ FileRegexFilter.js
@@ -0,0 +1,37 @@
+// @ts-check
+
+const config = require('./config/config.js');
+const fs = require('fs');
+const RegEx = new RegExp(config.regex, 'gi');
+
+fs.readdir(config.directory, (err, files) => {
+    // Filter Files
+    files = files.filter(str => str.search(config.fileFilter) != -1);
+    files.forEach(file => {
+        console.log(file);
+        // Read File and load contents
+        try{
+            
+        let data = fs.readFileSync(file, 'utf8');
+            //File is Loaded Exec the RegExp
+            if (data){
+                //Open a Stream to the output File
+                let outputStream = fs.createWriteStream('FilteredOutput.a', {flags: 'a'}).on('error', function(err){
+                    console.log(err);
+                });
+                
+                
+                let match = RegEx.exec(data);
+                while (match != null){
+                    //Write the Match to a file
+                    outputStream.write(`${match[0]}\n`);
+                    match = RegEx.exec(data);
+                }                
+                outputStream.close();
+            }
+        }catch(err){
+            console.error(err);
+        }
+        
+    });
+});
